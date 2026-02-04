ca72082d54bf728614f948a1dc823ba7ff8665e5
Lucas Calíope <lucascaliope2010@gmail.com>
2026-02-04 19:31:02 -0300

    redux no css

---
+++ src/containers/Produtos.tsx
@@ -0,0 +1,37 @@
+import { useSelector } from 'react-redux'
+import { Produto as ProdutoType } from '../App'
+import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
+import { RootState } from '../store'
+
+import * as S from './styles'
+
+const ProdutosComponent = () => {
+  const { data: produtos, isLoading } = useGetProdutosQuery()
+  const favoritos = useSelector((state: RootState) => state.carrinho.favoritos)
+
+  const produtoEstaNosFavoritos = (produto: ProdutoType) => {
+    const produtoId = produto.id
+    const IdsDosFavoritos = favoritos.map((f) => f.id)
+
+    return IdsDosFavoritos.includes(produtoId)
+  }
+
+  if (isLoading) return <h2>Carregando...</h2>
+
+  return (
+    <>
+      <S.Produtos>
+        {produtos?.map((produto) => (
+          <Produto
+            estaNosFavoritos={produtoEstaNosFavoritos(produto)}
+            key={produto.id}
+            produto={produto}
+          />
+        ))}
+      </S.Produtos>
+    </>
+  )
+}
+
+export default ProdutosComponent
